cdaa7543b5637afa375832cdbbc0a55bca72f0f6
stefaleon <stefaleon@gmail.com>
2018-02-15 15:44:50 +0200

    textInput

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    placeName: ''
+  }
+
+  placeNameChangedHandler = val =>  {
+    this.setState({
+      placeName: val
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-      <Text style={styles.titleText} onPress={this.onPressTitle}>
-          Ohai!!!
-          </Text>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <TextInput
+          style={{width: 300, borderColor: "black", borderWidth: 1}}
+          placeholder="An awesome place"
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangedHandler}
+        />
       </View>
     );
   }
@@ -18,15 +29,15 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
   titleText: {
     fontSize: 50,
     fontWeight: 'bold',
     color: 'red',
     marginLeft: 130
-
   },
 });
